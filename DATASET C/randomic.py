c960c0b30dd7fde7c1a14ed5b96e1f80e6f5e659
Valentina Pasqual <45206913+ValentinaPasqual@users.noreply.github.com>
2022-04-29 17:57:27 +0200

    Update randomic.py

--- DATASET C/randomic.py
+++ DATASET C/randomic.py
@@ -16,46 +16,51 @@ humans_list = [a['human'] for a in json_humans]
 num = [1,2,3,4,5]
 result = []
 
-string3 = str()
-string_final = str()
-for na in range(len(artists_list)):
-    n = random.choice(num)
-    x = 0
-    artist = artists_list[na]
-    string2 = str()
-    short_subj = artist.replace('http://www.wikidata.org/entity/', '')
-    while x <= n:
-        res = ()
-        obj = random.choice(humans_list)
-        statement = str('R-' + str(na) + '-' + str(x) + '-' + artist.replace('http://www.wikidata.org/entity/', ''))
-        res = (artist, obj, statement)
-        result.append(res)
-        short_obj = obj.replace('http://www.wikidata.org/entity/', '')
-        string = str()
-        string = string + """
-                    {\"mainsnak\": {
-                \"snaktype\": \"value\",
-                \"property\": \"P170\",
-                \"hash\": \"330a2a62e33dca172c13d2aec4565ef2aba54cb1\",
-                \"datavalue\": {
-                \"value\": {
-                        \"entity-type\": \"item\",
-                        \"numeric-id\": 170,
-                        \"id\":\""""+ short_obj +"""\"
-                },
-                \"type\": \"wikibase-entityid\"
-                },
-                \"datatype\": \"wikibase-item\"
-        },
-        \"type\": \"statement\",
-        \"id\": \"""" + statement + """\",
-        \"rank\": \"deprecated\"}"""
-        if x != n:
-            string = string + ','
-        string2 = string2 + string
-        x += 1
-    string3 = """{\"""" + short_subj + """\":{"id":\"""" + short_subj+ """\", \"claims\": {\"P170\":[""" + string2 + "]}}}"
-string_final = """{\"entities\": """ + string3 + "}"
 
-print(string_final)
-print('****************************')
+i = 0
+while i <= len(artists_list):
+    string3, string4, final_string = str(), str(), str()
+    for artist in artists_list[i:i+100]:
+        n = random.choice(num)
+        x = 0
+        string2 = str()
+        short_subj = artist.replace('http://www.wikidata.org/entity/', '')
+        while x <= n:
+            res = ()
+            obj = random.choice(humans_list)
+            statement = str('R-' + str(artists_list.index(artist)) + '-' + str(x) + '-' + artist.replace('http://www.wikidata.org/entity/', ''))
+            res = (artist, obj, statement)
+            result.append(res)
+            short_obj = obj.replace('http://www.wikidata.org/entity/', '')
+            string = str()
+            string = string + """
+                        {\"mainsnak\": {
+                    \"snaktype\": \"value\",
+                    \"property\": \"P170\",
+                    \"hash\": \"330a2a62e33dca172c13d2aec4565ef2aba54cb1\",
+                    \"datavalue\": {
+                    \"value\": {
+                            \"entity-type\": \"item\",
+                            \"numeric-id\": 170,
+                            \"id\":\""""+ short_obj +"""\"
+                    },
+                    \"type\": \"wikibase-entityid\"
+                    },
+                    \"datatype\": \"wikibase-item\"
+            },
+            \"type\": \"statement\",
+            \"id\": \"""" + statement + """\",
+            \"rank\": \"deprecated\"}"""
+            if x != n:
+                string = string + ','
+            string2 = string2 + string
+            x += 1
+        string3 = """\"""" + short_subj + """\":{"id":\"""" + short_subj+ """\", \"claims\": {\"P170\":[""" + string2 + "]}}"
+        if artists_list.index(artist) != i+99 :
+            string3 = string3 + ','
+        string4 = string4 + string3
+    string_final = """{\"entities\": {""" + string4 + "}}"
+    r = json.loads(string_final)
+    with open('C:/Users/Valentina/Documents/DHDK/DHARC/PhD/tesi_eduard/fake_statements/' + "file" + str(i) + '-' + str(i+100) + ".json", 'w') as outfile:
+        json.dump(r, outfile, indent=4)
+    i += 100
