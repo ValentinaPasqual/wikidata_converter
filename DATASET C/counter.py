b7cf2f4d01982071dbf3e8d638f80d18619c2811
Valentina Pasqual <45206913+ValentinaPasqual@users.noreply.github.com>
2022-04-29 18:22:50 +0200

    Create counter.py

---
+++ DATASET C/counter.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+directory_name = 'C:/Users/Valentina/Documents/DHDK/DHARC/PhD/tesi_eduard/fake_statements/'
+directory = os.fsencode(directory_name)
+
+
+def counter(json_file):
+    entities_list, statements_list = [], []
+    for entity in json_file['entities']:
+        entities_list.append(entity)
+        for predicate in json_file['entities'][entity]['claims']:
+            for claim in json_file['entities'][entity]['claims'][predicate]:
+                statements_list.append(claim['id'])
+    n_entities = len(entities_list)
+    n_statements = len(statements_list)
+    return n_entities, n_statements
+
+tot_ne, tot_ns = int(), int()
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".json"):
+        f = open(directory_name + '/' + filename, encoding='utf-8')
+        print(filename)
+        json_file = json.load(f) # input jsons
+        ne, ns = counter(json_file)
+        tot_ne += ne
+        tot_ns += ns
+        
+print(tot_ne, tot_ns)
