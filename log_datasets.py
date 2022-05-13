6cdd6ad5cfed5a2d1c1de394c3522a8a37c68c1e
ValentinaPasqual <valentina.pasqual@studio.unibo.it>
2022-05-13 16:24:34 +0200

    Update log_datasets.py

--- log_datasets.py
+++ log_datasets.py
@@ -26,6 +26,10 @@ def recursive_search_for_int(n, taken_numbers_list):
     else:
         return recursive_search_for_int(n, taken_numbers_list)
 
+def countfiles(path):
+    for dirpath, dirname, filename in os.walk(path):
+        return len(filename)
+
 def findfile(name, path):
     for dirpath, dirname, filename in os.walk(path):
         if name in filename:
@@ -35,75 +39,79 @@ def copy_chosen_files(n, input_path, output_path, file_start_name):
     i = 0
     taken_numbers_list = []
     while i < n:
-        random_number, taken_numbers_list = recursive_search_for_int(n, taken_numbers_list)
+        files_count = countfiles(input_path)
+        random_number, taken_numbers_list = recursive_search_for_int(files_count, taken_numbers_list)
         file_name = file_start_name + str(random_number) + '.json'
         file_path = findfile(file_name, input_path)
-        shutil.copy(file_path, output_path)
+        new_path = shutil.copy(file_path, output_path + file_name)
         i +=1
 
 
+# C:/Users/Valentina/Documents/DHDK/DHARC/PhD/tesi_eduard/dataset/dataset_sample
 
 
 ################################ SETTING UP INPUT DATA AND DIRECTORIES ################################
 
-# path of input data (jsons artworks 3'500'000)
-input_path = str(input('Enter input folder path:> '))
-#input_path = "C:/Users/Valentina/Documents/DHDK/DHARC/PhD/tesi_eduard/dataset/json_artworks_not_really/"
 
-# directory where to save new data
+input_path_artworks = str(input('Enter input artworks folder path:> '))
+input_path_random = str(input('Enter input random entities folder path:> '))
+input_path_fake = str(input('Enter input fake entities folder path:> '))
 root_dir = str(input('Enter output folder path:> '))
-#root_dir = "C:/Users/Valentina/Documents/DHDK/DHARC/PhD/tesi_eduard/dataset/"
-new_dir = make_directory(root_dir, 'log_data/')
-
-n_artworks_entities = 3537045
-n_of_artworks_jsons = 1999 # change with the number of json file in the input path repository
-
-################################ CALL FUNCTION FOR D1 = 3500ca ENTITIES ################################
-
-################################ CALL FUNCTION FOR D1 - ARTWORKS (ca 3500 Entities) ################################
-
-n_entities_d1 = int(n_artworks_entities / 1000)
-n_d1 = int(n_entities_d1 / 50)
-d1_output_path = make_directory(new_dir, 'D1/')
-
-print('#### starting filling dir D1 #####')
-print(n_entities_d1, 'selected artwork entities')
-copy_chosen_files(n_d1, input_path, d1_output_path, 'artwork')
-
-d1_entities_list = select_data_from_json(d1_output_path)
-d1_prepared_entities_list = wikidata_api_data_preparation(d1_entities_list)
-make_directory(d1_output_path, 'related_entities')
-wikidata_api_requestor(d1_prepared_entities_list, d1_output_path + 'related_entities/')
-
-################################ CALL FUNCTION FOR D1 - RUMOR (ca 3500 Entities) ################################
+new_dir = make_directory(root_dir, 'log_data\\')
 
 
 
-################################ CALL FUNCTION FOR D2 = 3500ca ENTITIES ################################
+def artworks_data(data_divisor, directory_name, input_path_artworks):
+    n_artworks_entities = 3537045
+    n_entities = int(n_artworks_entities / data_divisor)
+    n = int(n_entities / 50)
+    output_path = make_directory(new_dir, str(directory_name + '\\'))
 
-n_entities_d2 = int(n_artworks_entities / 100)
-n_d2 = int(n_entities_d2 / 50)
-print('#### starting filling dir D2 #####')
-print(n_entities_d2, 'selected artwork entities')
-d2_output_path = make_directory(new_dir, 'D2/')
-copy_chosen_files(n_d2, input_path, d2_output_path, 'artwork')
+    print('#### starting filling dir ' + directory_name + ' #####')
+    print(n_entities, 'selected artwork entities')
+    copy_chosen_files(n, input_path_artworks, output_path, 'artwork')
 
-d2_entities_list = select_data_from_json(d2_output_path)
-d2_prepared_entities_list = wikidata_api_data_preparation(d2_entities_list)
-make_directory(d2_output_path, 'related_entities')
-wikidata_api_requestor(d2_prepared_entities_list, d2_output_path + 'related_entities/')
+    #entities_list = select_data_from_json(output_path)
+    #prepared_entities_list = wikidata_api_data_preparation(entities_list)
+    #make_directory(output_path, 'related_entities')
+    #wikidata_api_requestor(prepared_entities_list, output_path + 'related_entities/')
+
+    return output_path
 
 
-################################ CALL FUNCTIONS FOR D3 = 350'000 ARTWORK ENTITIES ################################
+d1_path = artworks_data(1000, 'D1', input_path_artworks)
+d2_path = artworks_data(100, 'D2', input_path_artworks)
+#d3_path = artworks_data(10, 'D3', input_path_artworks)
 
-n_entities_d3 = int(n_artworks_entities / 10)
-n_d3 = int(n_entities_d3 / 50)
-print('#### starting filling dir D3 #####')
-print(n_entities_d3, 'selected artwork entities')
-d3_output_path = make_directory(new_dir, 'D3/')
-copy_chosen_files(n_d3, input_path, d3_output_path, 'artwork')
 
-d3_entities_list = select_data_from_json(d3_output_path)
-d3_prepared_entities_list = wikidata_api_data_preparation(d3_entities_list)
-make_directory(d3_output_path, 'related_entities')
-wikidata_api_requestor(d3_prepared_entities_list, d3_output_path + 'related_entities/')
+def random_data(data_divisor, output_path, input_path_random):
+    n_random_entities = 2999999
+    n_entities = int(n_random_entities / data_divisor)
+    n = int(n_entities / 50)
+
+    print('#### starting filling dir #####')
+    print(n_entities, 'selected random entities')
+    copy_chosen_files(n, input_path_random, output_path, 'entities')
+
+random_data(1000, d1_path, input_path_random)
+random_data(100, d2_path, input_path_random)
+#random_data(10, d3_path, input_path_random)
+
+def fake_data(data_divisor, output_path, input_path_fake):
+    n_fake_authors = int(996679 / data_divisor)
+    n_fake_creators = int(50000/ data_divisor)   # è sbagliato, per ora è a caso, perchè il dato non c'è su github
+    n_fake_locations = int(203236 / data_divisor)
+    n_authors = int(n_fake_authors / 200)
+    n_creators = int(n_fake_creators / 200)
+    n_locations = int(n_fake_locations / 200)
+    print('#### starting filling dir #####')
+    print(n_authors, 'selected fake authors entities')
+    print(n_creators, 'selected fake creators entities')
+    print(n_locations, 'selected fake locations entities')
+    fake_jsons_path = make_directory(output_path, 'fake_jsons')
+    copy_chosen_files(n_authors, input_path_random + 'authors/', fake_jsons_path, 'author')
+    copy_chosen_files(n_creators, input_path_random + 'creators/', fake_jsons_path, 'creator')
+    copy_chosen_files(n_locations, input_path_random + 'locations/', fake_jsons_path, 'location')
+
+fake_data(1000, d1_path, input_path_fake)
+
